ec7d92e688edc2eb30e7df1a3b6c2acccd6f755c
Kyushu21 <105261916+Kyushu21@users.noreply.github.com>
2023-11-07 15:38:53 -0700

    Add files via upload

---
+++ SkillSwap/app.js
@@ -0,0 +1,7 @@
+document.getElementById("login-button").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+    document.getElementById("register-button").addEventListener("click", function() {
+        window.location.href = "registro.html";
+   
+});
